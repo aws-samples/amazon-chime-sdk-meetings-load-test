0e4e73184cdfbb09022559c9506db5da6d750fe5
Ridip De <ridip@amazon.com>
2020-12-30 23:59:53 -0800

    Add optional methods to ClientController
    cr https://code.amazon.com/reviews/CR-42022448

--- src/ClientController.js
+++ src/ClientController.js
@@ -1,3 +1,5 @@
+import ClientLauncher from "../ClientLauncher";
+
 class ClientController {
   /*
   * This class is not being used currently as part of the seperation of the client and the launcher tasks. The methods in this class have however been implemented on the client side
@@ -131,8 +133,74 @@ class ClientController {
             return;
           }
         }
-      }, this.MIN_ACTIVE_TIME_MS * waitFactor);
+      }, 100000 * waitFactor);
     }
+  }
+
+  async startMeetingSession(page, meetingInfo, attendeeInfo, browserTab, threadId) {
+    if (workerData.threadId === threadId && page !== null) {
+      try {
+        const meetingId = meetingInfo.MeetingId;
+        const attendeeId = attendeeInfo.AttendeeId;
+        if (page !== null) {
+          const meetingStartStatus = await page.evaluate(
+            (attendeeId, meetingId, browserTab) => {
+              return new Promise((resolve, reject) => {
+                try {
+                  document.getElementById('inputMeeting').value = meetingId;
+                  document.getElementById('inputName').value = meetingId + ' : ' + browserTab + ' : ' + attendeeId;
+                  document.getElementById('authenticate').click();
+                  resolve('Success');
+                } catch (err) {
+                  resolve('Fail');
+                }
+              });
+            }, attendeeId, meetingId, browserTab);
+
+          if (meetingStartStatus === 'Success') {
+            this.support.log('Meeting start SUCCESS on tab # ', browserTab);
+            this.support.putMetricData('MeetingStartSuccess', 1);
+          } else {
+            this.support.log('Meeting start FAIL on tab # ', browserTab);
+            this.support.putMetricData('MeetingStartFail', 1);
+          }
+        }
+      } catch (err) {
+        this.support.error('Exception on page evaluate ' + err, browserTab);
+        this.support.putMetricData('MeetingStartFailPageEvaluate', 1);
+      }
+    }
+  }
+
+  async fetchMetricsFromBrowser(page) {
+    setTimeout(() => {
+      for (const [key, value] of Object.entries(page)) {
+        this.reportFetch[key] = setInterval(async () => {
+          this.support.putMetricData('Beacon-' + workerData.threadId, 1);
+          try {
+            if (page[key] !== null) {
+              const metricReport = await page[key].evaluate(() => app.metricReport);
+              if (
+                metricReport.meetingId || metricReport.attendeeId || metricReport.audioDecoderLoss || metricReport.audioPacketsReceived || metricReport.audioPacketsReceivedFractionLoss || metricReport.audioSpeakerDelayMs || metricReport.availableReceiveBandwidth || metricReport.availableSendBandwidth
+              ) {
+                const bodyHTML = await page[key].evaluate(() => document.body.innerText);
+                this.support.log(bodyHTML);
+                this.writeMetricsToFile(metricReport, metricReport.attendeeId, workerData.meetingsDirectory + '/' + metricReport.meetingId);
+              } else {
+                this.support.log('Metrics not received');
+                this.support.putMetricData('MeetingInactive', 1);
+                const bodyHTML = await page[key].evaluate(() => document.body.innerText);
+                this.support.log(bodyHTML);
+              }
+            } else {
+              this.support.log('Failed Metrics Reading');
+            }
+          } catch (err) {
+            this.support.error('Cannot retrieve Metrics from browser meeting ' + err.message);
+          }
+        }, 1000);
+      }
+    }, 180000);
   }
 
   writeMetricsToFile(webRTCStatReport, attendeeId, fileLocation) {
